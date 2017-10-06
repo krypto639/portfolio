63d10c021d363d9179a6027b018887d3cc747036
krypto639 <kyle.whyte639@gmail.com>
2017-10-06 17:16:58 +1300

    v1

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(function() {
+    $('.Letterpress').hover(function() {
+        $('.Display1').toggleClass('open');
+    });
+	$('.Letterpress').hover(function() {
+        $('.Display1').toggleClass('open');
+    });
+});
