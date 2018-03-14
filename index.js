aa63d28877e4ecae75493bf398aaf4224e28a73c
krypto639 <kyle.whyte639@gmail.com>
2018-03-14 15:27:07 +1300

    Version 2
    Functional update to site with more improvement to portfolio work and information

--- index.js
+++
@@ -1,9 +0,0 @@
-
-$(function() {
-    $('.Letterpress').hover(function() {
-        $('.Display1').toggleClass('open');
-    });
-	$('.Letterpress').hover(function() {
-        $('.Display1').toggleClass('open');
-    });
-});
